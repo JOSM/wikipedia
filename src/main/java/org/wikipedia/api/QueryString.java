a70d0d539a62e853133a29c11732bd3b2221aa15
Simon Legner <Simon.Legner@gmail.com>
2021-05-18 23:05:54 +0200

    Java 8: use Map.Entry.comparingByKey

--- src/main/java/org/wikipedia/api/QueryString.java
+++ src/main/java/org/wikipedia/api/QueryString.java
@@ -3,7 +3,6 @@ package org.wikipedia.api;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -64,7 +63,7 @@ public class QueryString {
     @Override
     public String toString() {
         return parameters.entrySet().stream()
-            .sorted(Comparator.comparing(Map.Entry::getKey))
+            .sorted(Map.Entry.comparingByKey())
             .map(it -> Utils.encodeUrl(it.getKey()) + "=" + Utils.encodeUrl(it.getValue()))
             .collect(Collectors.joining("&"));
     }
