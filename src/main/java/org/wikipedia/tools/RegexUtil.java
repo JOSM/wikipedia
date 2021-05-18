f6dd741ca050ad0ea1551ea456243a180b9445cd
Simon Legner <Simon.Legner@gmail.com>
2021-05-19 01:08:12 +0200

    Remove obsolete RegexUtil.isValidSiteId

--- src/main/java/org/wikipedia/tools/RegexUtil.java
+++ src/main/java/org/wikipedia/tools/RegexUtil.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 public class RegexUtil {
     private static final Pattern PROPERTY_ID_PATTERN = Pattern.compile("^P[1-9][0-9]{0,9}$");
     private static final Pattern Q_ID_PATTERN = Pattern.compile("^Q[1-9][0-9]{0,9}$");
-    private static final Pattern SITE_ID_PATTERN = Pattern.compile("^[a-z0-9_]+wiki$");
 
     public static final Pattern INTEGER_PATTERN = Pattern.compile("^[0-9]+$");
 
@@ -52,14 +51,4 @@ public class RegexUtil {
         }
     }
 
-    /**
-     * Validates that a given string matches "[a-z]{2,3}wiki".
-     * This has to be improved in the future to exactly allow only existing site IDs and allow other wikimedia sites
-     * other than wikipedias, but for now it's good enough.
-     * @param value the potential site ID to check
-     * @return {@code true} if the site ID is valid, {@code false} otherwise
-     */
-    public static boolean isValidSiteId(final String value) {
-        return value != null && SITE_ID_PATTERN.matcher(value).matches();
-    }
 }
